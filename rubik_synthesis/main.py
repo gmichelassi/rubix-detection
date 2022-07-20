607680bbaed35ab77519d7955e611f3de730a699
anabcuelbas <abcuelbas@gmail.com>
2022-07-20 16:12:56 -0300

    feat(synthesis): refactoring

--- rubik_synthesis/main.py
+++ rubik_synthesis/main.py
@@ -9,8 +9,6 @@ def run(rubik_colors: dict):
     window.color = color.black
     cubes = build_rubik_cube(rubik_colors)
 
-    rotation_helper = Entity()
-
     collider = Entity(model='cube', scale=3, collider='box', visible=False)
 
     def collider_input(key):
@@ -22,37 +20,53 @@ def run(rubik_colors: dict):
 
     collider.input = collider_input
 
-    def reset_rotation_helper():
-        [setattr(e, 'world_parent', scene) for e in cubes]
-        rotation_helper.rotation = (0, 0, 0)
+    rotation_helper = Entity()
 
-    def rotate_side(normal, direction=1, speed=1):
+    def reset_rotation():
+        for cube in cubes:
+            position, rotation = round(cube.world_position, 1), cube.world_rotation
+            cube.parent = scene
+            cube.position, cube.rotation = position, rotation
+
+        rotation_helper.rotation = 0
+
+    def rotate_side(normal, direction):
         if normal == Vec3(1, 0, 0):
-            [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.x > 0]
-            rotation_helper.animate('rotation_x', 90 * direction, duration=.2 * speed, curve=curve.linear)
+            for cube in cubes:
+                if cube.x > 0:
+                    cube.parent = rotation_helper
+            rotation_helper.animate('rotation_x', 90 * direction, duration=.2, curve=curve.linear)
         elif normal == Vec3(-1, 0, 0):
-            [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.x < 0]
-            rotation_helper.animate('rotation_x', -90 * direction, duration=.2 * speed, curve=curve.linear)
+            for cube in cubes:
+                if cube.x < 0:
+                    cube.parent = rotation_helper
+            rotation_helper.animate('rotation_x', -90 * direction, duration=.2, curve=curve.linear)
 
         elif normal == Vec3(0, 1, 0):
-            [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.y > 0]
-            rotation_helper.animate('rotation_y', 90 * direction, duration=.2 * speed, curve=curve.linear)
+            for cube in cubes:
+                if cube.y > 0:
+                    cube.parent = rotation_helper
+            rotation_helper.animate('rotation_y', 90 * direction, duration=.2, curve=curve.linear)
         elif normal == Vec3(0, -1, 0):
-            [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.y < 0]
-            rotation_helper.animate('rotation_y', -90 * direction, duration=.2 * speed, curve=curve.linear)
+            for cube in cubes:
+                if cube.y < 0:
+                    cube.parent = rotation_helper
+            rotation_helper.animate('rotation_y', -90 * direction, duration=.2, curve=curve.linear)
 
         elif normal == Vec3(0, 0, 1):
-            [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.z > 0]
-            rotation_helper.animate('rotation_z', -90 * direction, duration=.2 * speed, curve=curve.linear)
+            for cube in cubes:
+                if cube.z > 0:
+                    cube.parent = rotation_helper
+            rotation_helper.animate('rotation_z', -90 * direction, duration=.2, curve=curve.linear)
         elif normal == Vec3(0, 0, -1):
-            [setattr(e, 'world_parent', rotation_helper) for e in cubes if e.z < 0]
-            rotation_helper.animate('rotation_z', 90 * direction, duration=.2 * speed, curve=curve.linear)
+            for cube in cubes:
+                if cube.z < 0:
+                    cube.parent = rotation_helper
+            rotation_helper.animate('rotation_z', 90 * direction, duration=.2, curve=curve.linear)
 
-        invoke(reset_rotation_helper, delay=.22 * speed)
-
-        if speed:
-            collider.ignore_input = True
-            invoke(setattr, collider, 'ignore_input', False, delay=.24 * speed)
+        invoke(reset_rotation, delay=.22)
+        collider.ignore_input = True
+        invoke(setattr, collider, 'ignore_input', False, delay=.24)
 
 
 def main():
@@ -61,7 +75,4 @@ def main():
     app = Ursina()
     run(rubik_colors)
     EditorCamera()
-    pivot = Entity()
-    PointLight(parent=pivot, shadows=True)
-    AmbientLight(color=color.rgba(100, 100, 100, 0.000001))
     app.run()
